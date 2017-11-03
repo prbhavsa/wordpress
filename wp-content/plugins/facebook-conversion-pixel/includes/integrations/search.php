760802209ad9c9a867fe27f6d65738a2666b99bd
prbhasva <prbhavsa@my.bridgeport.edu>
2017-11-03 15:26:03 -0400

    new merge

--- wp-content/plugins/facebook-conversion-pixel/includes/integrations/search.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function fca_pc_search_integration() {
-	
-	if ( is_search() ) {
-		wp_localize_script( 'fca_pc_client_js', 'fcaPcSearchQuery', array( 'search_string' => get_search_query() ) );
-	}
-}
--- wp-content/plugins/ub-custom-login/ub-custom-login.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
- * Plugin Name: UB Custom Login Panel
- * Author: Pritesh Bhavsar
- */
-
